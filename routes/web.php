ef292d577582a606f83500bed63f7eb501d0879b
Muhammad Madni <madnikhan791@icloud.com>
2025-07-30 05:20:59 +0100

    Fix composer dependencies and remove problematic post-install scripts

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use Exception;
+
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/shop', [HomeController::class, 'shop'])->name('shop');
